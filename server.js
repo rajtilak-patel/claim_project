058255ef0816283bbd618afc6220159177cd74e5
Rajtilak patel <75354435+rajtilak-patel@users.noreply.github.com>
2025-08-03 22:35:57 +0530

    cors

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const http = require('http');
 const socketIo = require('socket.io');
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://claim-project-frontend-lwvb.vercel.app","*"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json()); // for JSON body
 app.use(express.urlencoded({ extended: true })); // for form-url-encoded
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
